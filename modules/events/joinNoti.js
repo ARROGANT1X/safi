c53c242df9237131b6caddf3e56c0e747d27a717
ARROGANT1X <alfakmmax@gmail.com>
2024-07-24 18:22:06 +0200

    Add files via upload

---
+++ modules/events/joinNoti.js
@@ -0,0 +1,82 @@
+module.exports.config = {
+    name: "joinNoti",
+    eventType: ["log:subscribe"],
+    version: "1.0.4",
+    credits: "Mirai Team",
+    description: "Thông báo bot hoặc người vào nhóm",
+    dependencies: {
+        "fs-extra": " "
+    }
+};
+
+module.exports.run = async function({ api, event, Users, Threads }) {
+   var fullYear = global.client.getTime("fullYear");
+  	var getHours = await global.client.getTime("hours");
+			var session = `${getHours < 3 ? "ن" : getHours < 8 ? "ح" : getHours < 11 ? "ه" : getHours < 16 ? "ق" : getHours < 23 ? "ث" : "ه"}`
+    const { join } = global.nodemodule["path"];
+    const { threadID } = event;
+  const { PREFIX } = global.config;
+    console.log(2)
+    if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+        console.log(1)
+        return api.sendMessage("⚜️== 「اتصال ناجح ✅」==⚜️", threadID, async () => {
+            let check = true;
+            while (check) {
+                setTimeout(() => check = false, 30 * 1000);
+                const threadData = (await Threads.getInfo(threadID)) || {};
+                if (threadData.hasOwnProperty("adminIDs")) {
+                    check = false;
+                    api.sendMessage(" ", threadID, (err, info) => {
+                        global.client.handleReply.push({
+                            name: "langChoose_0x01042022",
+                            messageID: info.messageID,
+                            adminIDs: threadData.adminIDs
+                        });
+                    });
+                }
+            }
+            api.changeNickname(`[ . ] • ${(!global.config.BOTNAME) ? "و" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+          	api.sendMessage(``, threadID);
+		}); 
+	}
+    else {
+        try {
+            const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+            let { threadName, participantIDs } = await api.getThreadInfo(threadID);
+
+            const threadData = global.data.threadData.get(parseInt(threadID)) || {};
+			const path = join(" ");
+			const pathGif = join(path, `hdfi2.jpg`);
+
+			var mentions = [], nameArray = [], memLength = [], i = 0;
+			
+			for (id in event.logMessageData.addedParticipants) {
+				const userName = event.logMessageData.addedParticipants[id].fullName;
+				nameArray.push(userName);
+				mentions.push({ tag: userName, id });
+				memLength.push(participantIDs.length - i++);
+
+				if (!global.data.allUserID.includes(id)) {
+					await Users.createData(id, { name: userName, data: {} });
+					global.data.userName.set(id, userName);
+					global.data.allUserID.push(id);
+				}
+			}
+			memLength.sort((a, b) => a - b);
+			
+			(typeof threadData.customJoin == "undefined") ? msg = " ⚜️=×= 「 اشعار 」=×=⚜️\n\n「{name}」: اهلا \n\nفي  شات  {threadName} \n{type}\n انا بوت كوشي في خدمتك...! ❤🩶" : msg = threadData.customJoin;
+			msg = msg
+			.replace(/\{name}/g, nameArray.join(', '))
+			.replace(/\{type}/g, (memLength.length > 1) ?  'các bạn' : 'ه')
+			.replace(/\{soThanhVien}/g, memLength.join(', '))
+			.replace(/\{threadName}/g, threadName);
+
+			if (existsSync(path)) mkdirSync(path, { recursive: true });
+
+			if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
+			else formPush = { body: msg, mentions }
+
+			return api.sendMessage(formPush, threadID);
+		} catch (e) { return console.log(e) };
+	}
+}
